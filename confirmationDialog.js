0dc8f98bd07a86fdea894317ba544a9d3ac55178
Joshua Gray Gotham <116634579+JoshGrayGotham@users.noreply.github.com>
2022-10-29 21:12:52 +0100

    Confirmation Popup & Modal
    Added html for popup and modal, along with js supporting opening and closing the modal. Added basic css for functionality

--- confirmationDialog.js
+++ confirmationDialog.js
@@ -1,1 +1,11 @@
-alert('JS file connected!');
+function togglePopup() {
+	document.getElementById("confirmation-modal").classList.toggle("show-modal");
+}
+
+function openPopup() {
+	togglePopup();
+}
+
+function closePopup() {
+	togglePopup();
+}
